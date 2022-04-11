4e458dc066607ae640e9cd2ebc61fa7170eb324f
LamaWaffle <matty.eberhard@gmail.com>
2022-04-11 18:50:53 +1000

    Initial creation and upload.

--- 100-Days-of-Code/App.py
+++ 100-Days-of-Code/App.py
@@ -14,3 +14,4 @@ print(len(last_name))               # len prints out the length of the string
 print(last_name[0])                 # [0] prints one of the characters from the string. Python starts with 0. name - would be 0123 , n is 0, a is 1.
 
 print(last_name.index("m"))         # index - gives a string a value?
+
